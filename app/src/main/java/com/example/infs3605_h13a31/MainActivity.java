987b62e22371c26323dc84c598cd2ed5ae77c25d
William Liu <william_liu01@hotmail.com>
2021-06-29 16:55:26 +1000

    Initiated login screen elements

--- app/src/main/java/com/example/infs3605_h13a31/MainActivity.java
+++ app/src/main/java/com/example/infs3605_h13a31/MainActivity.java
@@ -2,18 +2,44 @@ package com.example.infs3605_h13a31;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText loginEmail, loginPassword;
+    private Button loginSignin;
+    private TextView loginForgotPassword, loginRegister;
+    private ProgressBar loginProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPassword = findViewById(R.id.loginPassword);
+        loginSignin = findViewById(R.id.loginSignin);
+        loginSignin.setOnClickListener(this);
+        loginRegister = findViewById(R.id.loginRegister);
+        loginRegister.setOnClickListener(this);
+        loginForgotPassword = findViewById(R.id.loginForgotPassword);
+        loginForgotPassword.setOnClickListener(this);
+        loginProgressBar = findViewById(R.id.loginProgressBar);
 
 
+    }
 
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginRegister:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/infs3605_h13a31/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.infs3605_h13a31;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
