63f28123d2e5af9900f082a0631f34cab930bb16
William Liu <61264567+williamliuuu@users.noreply.github.com>
2021-06-29 11:19:56 +1000

    Add files via upload

---
+++ app/src/main/java/com/example/infs3605_h13a31/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.infs3605_h13a31;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
